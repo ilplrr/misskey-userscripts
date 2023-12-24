9d6801b274e29748ab97add2711aefc05be01e03
ilplrr <ilplrr193@gmail.com>
2023-12-24 11:27:16 +0900

    update: column を swap した時にも高さ変更可能化を実行

--- make-deck-column-height-resizable.user.js
+++ make-deck-column-height-resizable.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         make-deck-column-height-resizable
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://misskey.io/
@@ -54,79 +54,92 @@
     document.body.appendChild(style);
   })();
 
+  const makeColumnHeightResizable = (column) => {
+    column.querySelectorAll(`.${separatorClassName}`).forEach((e) => e.remove());
+    const stacks = column.children;
+
+    let targetSeparator = null;
+
+    let first = true;
+    [...stacks].forEach((stack) => {
+      if (stack.classList.contains(separatorClassName)) return;
+      if (first) return (first = false);
+
+      const sep = document.createElement('div');
+      sep.classList.add(separatorClassName);
+      // sep.style.backgroundColor = `hsl(${new Date().getSeconds() * 6} 100% 80%)`;
+      column.insertBefore(sep, stack);
+
+      sep.onmousedown = (event) => {
+        targetSeparator = event.target;
+        column.style.userSelect = 'none';
+
+        column.onmousemove = (ev) => {
+          if (!ev.buttons) {
+            column.style.userSelect = null;
+            column.onmousemove = null;
+            targetSeparator = null;
+            return;
+          }
+          if (!targetSeparator) return;
+
+          const findActiveStack = (propName) => {
+            let elm = targetSeparator[propName];
+            while (elm && !elm.classList.contains(miClassNames.stackActive)) {
+              elm = elm[propName];
+            }
+            return elm;
+          };
+          const a = findActiveStack('previousElementSibling');
+          const b = findActiveStack('nextElementSibling');
+          if (!a || !b) return;
+
+          const rectA = a.getBoundingClientRect();
+          const rectB = b.getBoundingClientRect();
+          // console.log('ra,rb', ra, rb);
+
+          const top = rectA.top;
+          const bottom = rectB.bottom;
+          const height = bottom - top;
+          const cur = ev.clientY;
+          // console.log('t,c,b,h', top, cur, bottom, height);
+
+          const weightA = Math.abs(cur - top) / height;
+          const weightB = Math.abs(cur - bottom) / height;
+          // console.log('weightA,weightB', weightA, weightB);
+
+          const origFlexGrowA = Number(a.style.flexGrow) || Number(window.getComputedStyle(a).flexGrow) || 1;
+          const origFlexGrowB = Number(b.style.flexGrow) || Number(window.getComputedStyle(b).flexGrow) || 1;
+          const flexGrowTotal = origFlexGrowA + origFlexGrowB;
+          // console.log('flex-grow(orig): a,b,total', origFlexGrowA, origFlexGrowB, flexGrowToal);
+
+          const flexGrowA = weightA * flexGrowTotal;
+          const flexGrowB = weightB * flexGrowTotal;
+          a.style.flexGrow = flexGrowA;
+          b.style.flexGrow = flexGrowB;
+          // console.log('flex-grow:a,b,sum', flexGrowA, flexGrowB, flexGrowB + flexGrowA);
+        };
+      };
+      sep.onmouseup = () => {
+        column.style.userSelect = null;
+        targetSeparator = null;
+        column.onmousemove = null;
+      };
+    });
+  };
+
   const makeColumnsHeightResizable = () => {
     columnsElm.querySelectorAll(`section.${miClassNames.column}`).forEach((column) => {
-      column.querySelectorAll(`.${separatorClassName}`).forEach((e) => e.remove());
-      const stacks = column.children;
-
-      let targetSeparator = null;
-
-      let first = true;
-      [...stacks].forEach((stack) => {
-        if (stack.classList.contains(separatorClassName)) return;
-        if (first) return (first = false);
-
-        const sep = document.createElement('div');
-        sep.classList.add(separatorClassName);
-        // sep.style.backgroundColor = `hsl(${new Date().getSeconds() * 6} 100% 80%)`;
-        column.insertBefore(sep, stack);
-
-        sep.onmousedown = (event) => {
-          targetSeparator = event.target;
-          column.style.userSelect = 'none';
-
-          column.onmousemove = (ev) => {
-            if (!ev.buttons) {
-              column.style.userSelect = null;
-              column.onmousemove = null;
-              targetSeparator = null;
-              return;
-            }
-            if (!targetSeparator) return;
-
-            const findActiveStack = (propName) => {
-              let elm = targetSeparator[propName];
-              while (elm && !elm.classList.contains(miClassNames.stackActive)) {
-                elm = elm[propName];
-              }
-              return elm;
-            };
-            const a = findActiveStack('previousElementSibling');
-            const b = findActiveStack('nextElementSibling');
-            if (!a || !b) return;
-
-            const rectA = a.getBoundingClientRect();
-            const rectB = b.getBoundingClientRect();
-            // console.log('ra,rb', ra, rb);
-
-            const top = rectA.top;
-            const bottom = rectB.bottom;
-            const height = bottom - top;
-            const cur = ev.clientY;
-            // console.log('t,c,b,h', top, cur, bottom, height);
-
-            const weightA = Math.abs(cur - top) / height;
-            const weightB = Math.abs(cur - bottom) / height;
-            // console.log('weightA,weightB', weightA, weightB);
-
-            const origFlexGrowA = Number(a.style.flexGrow) || Number(window.getComputedStyle(a).flexGrow) || 1;
-            const origFlexGrowB = Number(b.style.flexGrow) || Number(window.getComputedStyle(b).flexGrow) || 1;
-            const flexGrowTotal = origFlexGrowA + origFlexGrowB;
-            // console.log('flex-grow(orig): a,b,total', origFlexGrowA, origFlexGrowB, flexGrowToal);
-
-            const flexGrowA = weightA * flexGrowTotal;
-            const flexGrowB = weightB * flexGrowTotal;
-            a.style.flexGrow = flexGrowA;
-            b.style.flexGrow = flexGrowB;
-            // console.log('flex-grow:a,b,sum', flexGrowA, flexGrowB, flexGrowB + flexGrowA);
-          };
-        };
-        sep.onmouseup = () => {
-          column.style.userSelect = null;
-          targetSeparator = null;
-          column.onmousemove = null;
-        };
-      });
+      makeColumnHeightResizable(column);
+      new MutationObserver((muationsList) => {
+        loop: for (const mutation of muationsList) {
+          for (const node of mutation.addedNodes) {
+            if (node.classList.contains(separatorClassName)) continue;
+            makeColumnHeightResizable(column);
+            break loop;
+          }
+        }
+      }).observe(column, { childList: true });
     });
   };
 
